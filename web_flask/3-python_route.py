1d370d4859f86dc7813571e36a477da6782bf36e
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-01 17:41:53 +0100

    dynamic route with default variable

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""Create a flask app"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    """ Display 'hello HBNB' """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """ Display HBNB """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    '''Display C followed by a text'''
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("python/<text>", strict_slashes=False)
+def python_is(text='is cool'):
+    """Display python followed by a text"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
