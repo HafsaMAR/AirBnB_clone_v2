2bc7a092b70fc21efee80b2aaa5798d3ab26ab15
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-01 18:12:22 +0100

    html rendering

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,1 +1,45 @@
 #!/usr/bin/python3
+"""Create a flask app"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    """ Display 'hello HBNB' """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """ Display HBNB """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    '''Display C followed by a text'''
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is(text='is cool'):
+    """Display python followed by a text"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_text(n):
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def my_html(n):
+    return f'<H1>Number: {n}</H1>'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
