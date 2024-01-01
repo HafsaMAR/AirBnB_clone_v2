00102a37845812a265aa21276531f7d6318f359b
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-01 16:41:24 +0100

    Task1 two routes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Create a flask app"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    """ Display 'hello HBNB' """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """ Display HBNB """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
