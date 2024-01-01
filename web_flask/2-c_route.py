a4a8e9bc35888d798d709773efc7cd1caf0720ca
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-01 16:54:59 +0100

    dynamic route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,7 @@ def HBNB():
 @app.route("/c/<text>", strict_slashes=False)
 def c_is_fun(text):
     '''Display C followed by a text'''
-    return f"C text.replace('_', ' ')"
+    return f"C {text.replace('_', ' ')}"
 
 
 if __name__ == "__main__":
