fc8fa6f0bc010f949b2cc0e34afe0a6344755034
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-01 18:22:43 +0100

    render_template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Create a flask app"""
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -38,7 +38,7 @@ def number_text(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def my_html(n):
-    return f'<H1>Number: {n}</H1>'
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
