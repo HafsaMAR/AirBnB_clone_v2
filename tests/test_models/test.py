264bf07ddeee5c718c52698e354f05b59f7d50db
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-01 19:50:54 +0100

    update

--- tests/test_models/test.py
+++ tests/test_models/test.py
@@ -1,11 +1,1 @@
 #!/usr/bin/python3
-import unittest
-from console import create
-
-
-class testConsole(unittest.TestCase):
-    """testing console methods """
-
-    def test_create_object(self):
-        """this func test the create """
-        
